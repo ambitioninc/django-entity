3dc8433252a2993a74421addc82e7164d0f8d0fe
Wes Kendall <wes.kendall@tryambition.com>
2014-05-24 12:35:52 -0400

    did minor optimizations to entity syncing

--- entity/sync.py
+++ entity/sync.py
@@ -29,7 +29,7 @@ def turn_on_syncing():
     post_bulk_operation.connect(bulk_operation_signal_handler, dispatch_uid='bulk_operation_signal_handler')
 
 
-def sync_entity(model_obj, is_deleted):
+def sync_entity(model_obj, is_deleted, entity_cache=None):
     """
     Given a model object, either delete it from the entity table (if
     is_deleted=False) or update its entity values and relationships.
@@ -41,39 +41,44 @@ def sync_entity(model_obj, is_deleted):
         # before hand
         Entity.objects.filter(entity_type=entity_type, entity_id=model_obj.id).delete()
     else:
-        # Create or update the entity
-        entity_meta = model_obj.get_entity_meta()
-        is_active = model_obj.is_entity_active()
-        entity = Entity.objects.upsert(
-            entity_type=entity_type, entity_id=model_obj.id, updates={
-                'entity_meta': entity_meta,
-                'is_active': is_active,
-            })[0]
+        entity_cache = entity_cache if entity_cache is not None else {}
 
-        # Delete all of the existing relationships super to this entity
-        sync(entity.super_relationships.all(), [
-            EntityRelationship(
-                super_entity=sync_entity(super_model_obj, False),
-                sub_entity=entity,
-            )
-            for super_model_obj in model_obj.get_super_entities()
-        ], ['super_entity_id', 'sub_entity_id'])
+        if not entity_cache.get((entity_type, model_obj.id)):
+            # Create or update the entity
+            entity = Entity.objects.upsert(
+                entity_type=entity_type, entity_id=model_obj.id, updates={
+                    'entity_meta': model_obj.get_entity_meta(),
+                    'is_active': model_obj.is_entity_active(),
+                })[0]
 
-        return entity
+            # Delete all of the existing relationships super to this entity
+            sync(entity.super_relationships.all(), [
+                EntityRelationship(
+                    super_entity=sync_entity(super_model_obj, False, entity_cache),
+                    sub_entity=entity,
+                )
+                for super_model_obj in model_obj.get_super_entities()
+            ], ['super_entity_id', 'sub_entity_id'])
+
+            entity_cache[(entity_type, model_obj.id)] = entity
+
+        return entity_cache[(entity_type, model_obj.id)]
 
 
 def sync_entities():
     """
     Sync all entities in a project.
     """
+    # Instantiate a cache that will be used by all calls to sync_entity
+    entity_cache = {}
+
     # Loop through all entities that inherit EntityModelMixin and sync the entity.
     entity_models = [model_class for model_class in models.get_models() if issubclass(model_class, EntityModelMixin)]
     for entity_model in entity_models:
-        model_obj_ids = []
-        for model_obj in entity_model.objects.all():
-            sync_entity(model_obj, False)
-            model_obj_ids.append(model_obj.id)
+        model_objs = list(entity_model.objects.all())
+        for model_obj in model_objs:
+            sync_entity(model_obj, False, entity_cache)
 
         # Delete any existing entities that are not in the model obj table
         Entity.objects.filter(entity_type=ContentType.objects.get_for_model(entity_model)).exclude(
-            entity_id__in=model_obj_ids).delete()
+            entity_id__in=(model_obj.id for model_obj in model_objs)).delete()
