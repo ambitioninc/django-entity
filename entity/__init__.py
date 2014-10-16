16f2d5be5dee3133e63534f7c65e8d2f61608bb7
Rob deCarvalho <robdmc@gmail.com>
2014-10-16 16:53:19 -0400

    adding entity_kind to __init__.py

--- entity/__init__.py
+++ entity/__init__.py
@@ -2,4 +2,4 @@
 from .config import EntityConfig, entity_registry, register_entity
 from .version import __version__
 from .tasks import SyncEntitiesTask
-from .models import Entity, EntityRelationship, turn_on_syncing, turn_off_syncing, sync_entities
+from .models import Entity, EntityKind, EntityRelationship, turn_on_syncing, turn_off_syncing, sync_entities
