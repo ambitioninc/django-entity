5febf10b5d7edeaa1dbe033611645d9bc2ce74bd
Wes Okes <wes.okes@gmail.com>
2016-02-17 11:44:02 -0500

    do not import models in init

--- entity/__init__.py
+++ entity/__init__.py
@@ -1,7 +1,6 @@
 # flake8: noqa
 from .config import EntityConfig, entity_registry, register_entity
 from .version import __version__
-from .models import Entity, EntityKind, EntityRelationship
 from .signal_handlers import turn_on_syncing, turn_off_syncing
 from .sync import sync_entities
 
