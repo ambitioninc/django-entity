0603ea37e6a4b06a54737b6aea9ee364529d4f3d
Wes Okes <wes.okes@gmail.com>
2017-12-08 17:11:49 -0500

    on_delete in main migrations

--- entity/migrations/0001_initial.py
+++ entity/migrations/0001_initial.py
@@ -41,8 +41,8 @@ class Migration(migrations.Migration):
             name='EntityRelationship',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('sub_entity', models.ForeignKey(related_name='super_relationships', to='entity.Entity')),
-                ('super_entity', models.ForeignKey(related_name='sub_relationships', to='entity.Entity')),
+                ('sub_entity', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='super_relationships', to='entity.Entity')),
+                ('super_entity', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='sub_relationships', to='entity.Entity')),
             ],
             options={
             },
