6c5b0900290caff229c6df1362d5c538c3a63a4c
Wes Okes <wes.okes@gmail.com>
2018-03-29 15:35:41 -0400

    tox and jsonfield change

--- setup.py
+++ setup.py
@@ -19,9 +19,9 @@ def get_version():
 
 
 install_requires = [
-    'Django>=1.11',
-    'django-activatable-model>=1.0.0',
-    'django-manager-utils>=1.0.0',
+    'Django>=1.10',
+    'django-activatable-model>=1.1.0',
+    'django-manager-utils>=1.1.1',
     'jsonfield>=0.9.20',
     'python3-utils>=0.3',
 ]
@@ -44,6 +44,8 @@ setup(
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Intended Audience :: Developers',
