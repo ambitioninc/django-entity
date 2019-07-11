8ef5978578d05720799ec10ba64d3243db10d326
Wes Okes <wes.okes@gmail.com>
2019-07-10 20:00:22 -0400

    version support

--- setup.py
+++ setup.py
@@ -46,10 +46,8 @@ setup(
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
