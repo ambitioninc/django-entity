717f7aaec86124aa2aed3ea21f7c31353bc62319
Wes Okes <wes.okes@gmail.com>
2019-07-11 14:48:01 -0400

    requirements

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ def get_version():
 
 install_requires = [
     'Django>=2.0',
-    'django-activatable-model>=1.1.0',
-    'django-manager-utils>=1.3.2',
+    'django-activatable-model>=1.2.1',
+    'django-manager-utils>=1.4.0',
     'jsonfield>=0.9.20',
     'python3-utils>=0.3',
     'wrapt>=1.10.5'
