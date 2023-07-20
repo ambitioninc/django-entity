ec55f3a2ea1c149d5872e2e400c12cec778a43b2
Jeffrey Cross <jeffrey.cross@ambition.com>
2023-07-20 15:28:06 -0400

    Correction

--- entity/migrations/0001_initial.py
+++ entity/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('display_name', models.TextField(db_index=True, blank=True)),
                 ('entity_id', models.IntegerField()),
-                ('entity_meta', db.models.JSONField(null=True)),
+                ('entity_meta', models.JSONField(null=True)),
                 ('is_active', models.BooleanField(default=True, db_index=True)),
             ],
             options={
