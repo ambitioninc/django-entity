8663e95139da04830637b57e40e2fc71f9005373
Wes Okes <somewes@gmail.com>
2023-10-03 17:12:49 -0400

    update publish.py

--- publish.py
+++ publish.py
@@ -1,10 +1,8 @@
 import subprocess
 
 subprocess.call(['rm', '-r', 'dist/'])
-subprocess.call(['pip', 'install', 'wheel'])
-subprocess.call(['pip', 'install', 'twine'])
-subprocess.call(['python', 'setup.py', 'clean', '--all'])
-subprocess.call(['python', 'setup.py', 'register', 'sdist', 'bdist_wheel'])
+subprocess.call(['python', '-m', 'pip', 'install', 'build', 'twine'])
+subprocess.call(['python', '-m', 'build'])
+subprocess.call(['twine', 'check', 'dist/*'])
 subprocess.call(['twine', 'upload', 'dist/*'])
 subprocess.call(['rm', '-r', 'dist/'])
-subprocess.call(['rm', '-r', 'build/'])
