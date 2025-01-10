afedac7a01283cdf776716494295584f2151491b
Ryan Bales <ryan.bales@ambition.com>
2025-01-10 14:17:40 -0500

    dropping support for python versions older than 3.9

--- setup.py
+++ setup.py
@@ -38,8 +38,6 @@ setup(
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
